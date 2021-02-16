733691a4b7d8bf6eb4c1439a61a9c2a4061113fb
santoshkumarbvp <54077829+santoshkumarbvp@users.noreply.github.com>
2021-02-16 13:01:14 +0100

    template_with_prediction requirement

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('bootstrap.html')
+
+
+@app.route('/predict')
+def index2():
+    prediction_value = predict_f()
+    # if news is None:
+    #     print('taken1', news)
+    # else:
+    #     print('noa',news)
+    return render_template('bootstrap_with_prediction.html', prediction= f'Expected variation in NIFTY50 stock exchange would be: {prediction_value}' )
+
+# check
+def predict_f():
+    if int(np.random.random()*10)%2 ==0:
+        num = - round(np.random.random()*1000,2)
+        return (num)
+    else:
+        num = round(np.random.random()*1000,2)
+        return (num)
 
 if __name__ == '__main__':
  app.run()
